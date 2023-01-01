04fb2f6468878fa0b6ee42f51ca94cc9f400e59d
G_Dychala <g.dychala@gmail.com>
2023-01-01 22:45:49 +0100

    added loading map on start

--- skrypt.js
+++ skrypt.js
@@ -1,5 +1,20 @@
 const tab_of_nodes=[{lat:52.2007456, lon:22.1184492},{lat:52.1998836, lon:22.1249418},{ lat:52.1993589, lon:22.1288888},{lat:52.1988388, lon:22.1327648},{ lat:52.1986125, lon:22.1343779},{lat:52.1983405, lon:22.1361401}, {lat:52.1981399, lon:22.1373274}, {lat:52.1980288, lon:22.1379153}, {lat:52.1978862, lon:22.1386388}, {lat:52.1976890, lon:22.1395547}, {lat:52.1974749, lon:22.1404566}, {lat:52.1972870, lon:22.1411863}, {lat:52.1970476, lon:22.1420406}, {lat:52.1967565, lon:22.1429697}, {lat:52.1964268, lon:22.1439218}, {lat: 52.1961457, lon:22.1446623}, {lat:52.1958780, lon:22.1453243}, {lat:52.1955795, lon:22.1460046}, {lat: 52.1952665, lon:22.1466613}, {lat:52.1949487, lon:22.1472878}, {lat:52.1945679, lon:22.1479882}, {lat:52.1941343, lon:22.1487370}, {lat:52.1934315, lon:22.1498092}, {lat:52.1930782, lon:22.1502979}, {lat:52.1925984, lon:22.1509201}, {lat:52.1918976, lon:22.1517484}, {lat:52.1913275, lon:22.1523438}, {lat:52.1907375, lon:22.1529081}, {lat:52.1901385, lon:22.1534064}, {lat:52.1898360, lon:22.1536457}, {lat:52.1895253, lon:22.1538778}, {lat:52.1891921, lon:22.1541099}, {lat:52.1888072, lon:22.1543517}, {lat:52.1880993, lon:22.1547624}, {lat:52.1875397, lon:22.1550271}, {lat:52.1873028, lon:22.1551310}, {lat:52.1867524, lon:22.1553504}, {lat:52.1862283, lon:22.1555301}, {lat:52.1858968, lon:22.1556331}, {lat:52.1855904, lon:22.1557160}, {lat:52.1849430, lon:22.1558625}, {lat: 52.1846820, lon:22.1559152}, {lat:52.1841732, lon:22.1559949}, {lat:52.1837202, lon:22.1560538}, {lat:52.1826075, lon:22.1561538}, {lat:52.1814927, lon:22.1562151}, {lat:52.1812445, lon:22.1562256}, {lat:52.1798992, lon:22.1562687}, {lat:52.1788548, lon:22.1563005}, {lat:52.1778425, lon:22.1563819}, {lat:52.1773734, lon:22.1564358}, {lat:52.1769845, lon:22.1564964}, {lat:52.1766476, lon:22.1565625}, {lat:52.1762327, lon:22.1566592}, {lat:52.1757863, lon:22.1567812}, {lat:52.1752929, lon:22.1569394}, {lat:52.1747522, lon:22.1571374}, {lat:52.1742559, lon:22.1573549}, {lat:52.1737469, lon:22.1576002}, {lat:52.1735021, lon:22.1577284}, {lat:52.1732558, lon:22.1578635}, {lat:52.1728212, lon:22.1581206}, {lat:52.1723946, lon:22.1583971}, {lat:52.1719740, lon:22.1586892}, {lat:52.1715411, lon:22.1590086}, {lat:52.1712064, lon:22.1592823}, {lat:52.1708786, lon:22.1595624}, {lat:52.1703172, lon:22.1600749}, {lat:52.1697383, lon:22.1606562}, {lat:52.1692468, lon:22.1612011}, {lat:52.1687737, lon:22.1617572}, {lat:52.1682982, lon:22.1623629}, {lat:52.1678934, lon:22.1629175}, {lat:52.1674734, lon:22.1635362}, {lat:52.1670927, lon:22.1641213}, {lat:52.1667994, lon:22.1646104}, {lat:52.1662803, lon:22.1653200}, {lat:52.1653801, lon:22.1671257}, {lat:52.1647413, lon:22.1686007}, {lat:52.1639448, lon:22.1708716}, {lat:52.1636423, lon:22.1718303}, {lat:52.1633721, lon:22.1728000}, {lat:52.1629563, lon:22.1745520}, {lat:52.1625823, lon:22.1762964}, {lat:52.1622444, lon:22.1779717}, {lat:52.1617398, lon:22.1802750}, {lat:52.1612795, lon:22.1822030}, {lat:52.1607639, lon:22.1839150}, {lat:52.1599844, lon:22.1859466}, {lat:52.1590714, lon:22.1878982}, {lat:52.1571705, lon:22.1908121}, {lat:52.1560091, lon:22.1921068}, {lat:52.1547596, lon:22.1932250}, {lat:52.1529775, lon:22.1946199}, {lat:52.1512343, lon:22.1960506}, {lat:52.1498841, lon:22.1974870}, {lat:52.1488108, lon:22.1989454}, {lat:52.1478950, lon:22.2005087}, {lat:52.1470560, lon:22.2021750}, {lat:52.1462088, lon:22.2040820}, {lat:52.1455451, lon:22.2061470}, {lat:52.1450062, lon:22.2080929}, {lat:52.1442596, lon:22.2108494}, {lat:52.1438630, lon:22.2124625}, {lat:52.1434199, lon:22.2145993}, {lat:52.1430609, lon:22.2171937}, {lat:52.1428884, lon:22.2188682}, {lat:52.1428445, lon:22.2194370}, {lat:52.1427946, lon:22.2201641}, {lat:52.1427444, lon:22.2211671}, {lat:52.1427175, lon:22.2222249}, {lat:52.1427108, lon:22.2227334}, {lat:52.1427138, lon:22.2237561}, {lat:52.1427284, lon:22.2247087}, {lat:52.1427548, lon:22.2256276}, {lat:52.1428146, lon:22.2274766}, {lat:52.1428389, lon:22.2281938}, {lat:52.1428683, lon:22.2290393}, {lat:52.1429308, lon:22.2308297}, {lat:52.1429628, lon:22.2317408}, {lat:52.1429992, lon:22.2328074}, {lat:52.1430264, lon:22.2337057}, {lat:52.1430631, lon:22.2354606}, {lat:52.1430736, lon:22.2363189}, {lat:52.1430799, lon:22.2371613}, {lat:52.1430764, lon:22.2381660}, {lat:52.1430594, lon:22.2391782}, {lat:52.1430483, lon:22.2398701}, {lat:52.1430303, lon:22.2404977}, {lat:52.1430154, lon:22.2408942}, {lat:52.1429880, lon:22.2414703}, {lat:52.1429242, lon:22.2425230}, {lat:52.1428400, lon:22.2436602}, {lat:52.1427562, lon:22.2446125}, {lat:52.1427352, lon:22.2448200}, {lat:52.1427108, lon:22.2450242}, {lat:52.1426388, lon:22.2456329}, {lat:52.1425869, lon:22.2460478}, {lat:52.1425204, lon:22.2465656}, {lat:52.1424477, lon:22.2470800}, {lat:52.1422216, lon:22.2485906}, {lat:52.1419348, lon:22.2503665}];
 
+var map = L.map('map').setView([52.1713402, 22.1844079], 12);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        var polygon = L.polygon([[52.2027456, 22.1164492],
+                                 [52.2027456, 22.2523665],
+                                 [52.1399348, 22.2523665],
+                                 [52.1399348, 22.1164492]]).addTo(map);
+
+ map.addControl(new L.Control.Fullscreen({
+            title: {
+                'false': 'widok pełnoekranowy',
+                'true': 'wyjdź z widoku pełnoekranowego'
+            }
+        }));
 
 //lokalizacja 
 function lokalizuj(){
@@ -56,15 +71,14 @@ let x = position.coords.latitude;
 let y = position.coords.longitude;
 let accu = position.coords.accuracy.toFixed(2);
 let k;
-let popr = 1.00004;
 
 for (i=0; i<tab_of_nodes.length-1; i++){  
          point1 = {latitude:tab_of_nodes[i].lat, longitude:tab_of_nodes[i].lon};
         point2 = {latitude:tab_of_nodes[i+1].lat, longitude:tab_of_nodes[i+1].lon};
 //if(x<=tab_of_nodes[i+1].lat&&y>=tab_of_nodes[i+1].lon){
         total = total+distance();
-        total = popr*total;
-       // console.log(distance());
+        total = total + 0.0002588;
+        console.log(total);
         k=i;
      //   }
        // console.log(k);
@@ -72,7 +86,7 @@ for (i=0; i<tab_of_nodes.length-1; i++){
 
     point1 = {latitude:tab_of_nodes[k+1].lat, longitude:tab_of_nodes[k+1].lon};
     point2 = {latitude:x, longitude:y};
-    total = total + popr*distance();
+    total = total + distance();
     //console.log(distance());
     total = total.toFixed(3);
     let km = total.toString().replace('.',' + ');
@@ -80,18 +94,11 @@ for (i=0; i<tab_of_nodes.length-1; i++){
     document.getElementById('odczyt').innerHTML = km;
     document.getElementById('dok').innerHTML = accu;
 
-    var map = L.map('map').setView([x, y], 18);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-        var marker = L.marker([x, y]).addTo(map);
-        marker.bindPopup("<b>aktuala lokalizacja</b>").openPopup();
-        map.addControl(new L.Control.Fullscreen({
-            title: {
-                'false': 'widok pełnoekranowy',
-                'true': 'wyjdź z widoku pełnoekranowego'
-            }
-        }));
+     map.flyTo([x, y], 18);
+    
+    var marker = L.marker([x, y]).addTo(map);
+    marker.bindPopup("<b>aktuala lokalizacja</b>").openPopup();
+        
 
 }
 
