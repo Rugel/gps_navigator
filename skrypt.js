c8e8a6b2a217e6f869bc0c0db021774a72dc9292
G_Dychala <g.dychala@gmail.com>
2023-03-04 15:21:23 +0100

    small changes of displaing

--- skrypt.js
+++ skrypt.js
@@ -1,4 +1,3 @@
-
 //tablice punktów lat i lon
 const latlonP = [[52.2007456, 22.1184492], [52.1998836, 22.1249418], [52.1993589, 22.1288888], [52.1988388, 22.1327648], [52.1986125, 22.1343779], [52.1983405, 22.1361401], [52.1981399, 22.1373274], [52.1980288, 22.1379153], [52.1978862, 22.1386388], [52.1976890, 22.1395547], [52.1974749, 22.1404566], [52.1972870, 22.1411863], [52.1970476, 22.1420406], [52.1967565, 22.1429697], [52.1964268, 22.1439218], [52.1961457, 22.1446623], [52.1958780, 22.1453243], [52.1955795, 22.1460046], [52.1952665, 22.1466613], [52.1949487, 22.1472878], [52.1945679, 22.1479882], [52.1941343, 22.1487370], [52.1934315, 22.1498092], [52.1930782, 22.1502979], [52.1925984, 22.1509201], [52.1918976, 22.1517484], [52.1913275, 22.1523438], [52.1907375, 22.1529081], [52.1901385, 22.1534064], [52.1898360, 22.1536457], [52.1895253, 22.1538778], [52.1891921, 22.1541099], [52.1888072, 22.1543517], [52.1880993, 22.1547624], [52.1875397, 22.1550271], [52.1873028, 22.1551310], [52.1867524, 22.1553504], [52.1862283, 22.1555301], [52.1858968, 22.1556331], [52.1855904, 22.1557160], [52.1849430, 22.1558625], [52.1846820, 22.1559152], [52.1841732, 22.1559949], [52.1837202, 22.1560538], [52.1826075, 22.1561538], [52.1814927, 22.1562151], [52.1812445, 22.1562256], [52.1798992, 22.1562687], [52.1788548, 22.1563005], [52.1778425, 22.1563819], [52.1773734, 22.1564358], [52.1769845, 22.1564964], [52.1766476, 22.1565625], [52.1762327, 22.1566592], [52.1757863, 22.1567812], [52.1752929, 22.1569394], [52.1747522, 22.1571374], [52.1742559, 22.1573549], [52.1737469, 22.1576002], [52.1735021, 22.1577284], [52.1732558, 22.1578635], [52.1728212, 22.1581206], [52.1723946, 22.1583971], [52.1719740, 22.1586892], [52.1715411, 22.1590086], [52.1712064, 22.1592823], [52.1708786, 22.1595624], [52.1703172, 22.1600749], [52.1697383, 22.1606562], [52.1692468, 22.1612011], [52.1687737, 22.1617572], [52.1682982, 22.1623629], [52.1678934, 22.1629175], [52.1674734, 22.1635362], [52.1670927, 22.1641213], [52.1667994, 22.1646104], [52.1662803, 22.1653200], [52.1653801, 22.1671257], [52.1647413, 22.1686007], [52.1639448, 22.1708716], [52.1636423, 22.1718303], [52.1633721, 22.1728000], [52.1629563, 22.1745520], [52.1625823, 22.1762964], [52.1622444, 22.1779717], [52.1617398, 22.1802750], [52.1612795, 22.1822030], [52.1607639, 22.1839150], [52.1599844, 22.1859466], [52.1590714, 22.1878982], [52.1571705, 22.1908121], [52.1560091, 22.1921068], [52.1547596, 22.1932250], [52.1529775, 22.1946199], [52.1512343, 22.1960506], [52.1498841, 22.1974870], [52.1488108, 22.1989454], [52.1478950, 22.2005087], [52.1470560, 22.2021750], [52.1462088, 22.2040820], [52.1455451, 22.2061470], [52.1450062, 22.2080929], [52.1442596, 22.2108494], [52.1438630, 22.2124625], [52.1434199, 22.2145993], [52.1430609, 22.2171937], [52.1428884, 22.2188682], [52.1428445, 22.2194370], [52.1427946, 22.2201641], [52.1427444, 22.2211671], [52.1427175, 22.2222249], [52.1427108, 22.2227334], [52.1427138, 22.2237561], [52.1427284, 22.2247087], [52.1427548, 22.2256276], [52.1428146, 22.2274766], [52.1428389, 22.2281938], [52.1428683, 22.2290393], [52.1429308, 22.2308297], [52.1429628, 22.2317408], [52.1429992, 22.2328074], [52.1430264, 22.2337057], [52.1430631, 22.2354606], [52.1430736, 22.2363189], [52.1430799, 22.2371613], [52.1430764, 22.2381660], [52.1430594, 22.2391782], [52.1430483, 22.2398701], [52.1430303, 22.2404977], [52.1430154, 22.2408942], [52.1429880, 22.2414703], [52.1429242, 22.2425230], [52.1428400, 22.2436602], [52.1427562, 22.2446125], [52.1427352, 22.2448200], [52.1427108, 22.2450242], [52.1426388, 22.2456329], [52.1425869, 22.2460478], [52.1425204, 22.2465656], [52.1424477, 22.2470800], [52.1422216, 22.2485906], [52.1419348, 22.2503665]];
 
@@ -9,18 +8,18 @@ latlonL.reverse();
 //dodanie mapy
 var map = L.map('map').setView([52.1713402, 22.1844079], 12);
 
-let sat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+let sat = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 })
 
-let str = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+let str = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 })
 
-let osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+let osm = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 });
 
 osm.addTo(map);
@@ -102,7 +101,7 @@ let distance = function (p1, p2) {
     return radius * c;
 }
 
-const result = document.getElementById('h3');
+const result = document.getElementById('result');
 const button = document.getElementById('button');
 
 const options = {
@@ -150,8 +149,8 @@ function start() {
             point2 = { latitude: x, longitude: y };
             total = distance(point1, point2).toFixed(3);
             result.style.color = 'red';
-            result.innerHTML = `odczyt spoza zakresu terenu budowy - odległość do punktu km 0 + 000 wynosi: <mark><b>${total}</b></mark> km`;
-            result.style.fontSize = '1.5rem';
+            result.style.fontSize = '1.3rem';
+            result.innerHTML = `odczyt spoza zakresu terenu budowy - odległość do punktu km 0 + 000 wynosi: <span style="color:blue">${total}</span> km`;
             polyline ? map.removeLayer(polyline) : null;
             polyline = L.polyline([[x, y], [(latlonL[0][1] + latlonP[0][1]) / 2, (latlonL[0][0] + latlonP[0][0]) / 2]], { color: "orange", weight: 1 }).addTo(map);
             deco ? map.removeLayer(deco) : null;
@@ -168,7 +167,6 @@ function start() {
         circle = L.circleMarker([x, y], { color: '#2dabab', fillOpacity: .2 }).addTo(map).bindPopup(subline_length ? 'km ' + subline_length : total + ' km');
 
         //proj4
-
         // Zdefiniowanie systemów odniesienia współrzędnych
         proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs");
         proj4.defs("EPSG:2180", "+proj=tmerc +lat_0=0 +lon_0=19 +k=0.9993 +x_0=500000 +y_0=-5300000 +ellps=GRS80 +units=m +no_defs");
@@ -176,13 +174,13 @@ function start() {
         // Funkcja przekształcająca współrzędne z GPS na PL-1992
         function gpsToPl1992(longitude, latitude) {
             const gpsCoords = [longitude, latitude];
-            const pl1992Coords = proj4("EPSG:4326", "EPSG:2180", gpsCoords); // Poprawienie tego fragmentu
+            const pl1992Coords = proj4("EPSG:4326", "EPSG:2180", gpsCoords);
             return pl1992Coords;
         }
         const pl1992Coords = gpsToPl1992(y, x);
         const x1992 = pl1992Coords[0];
         const y1992 = pl1992Coords[1];
-        const geoLink = `<a href="https://mapy.geoportal.gov.pl/mobile/#zoom&x=${x1992}&y=${y1992}&minLevel=18&gpsPosition=true&1677663465149" target="blank">Więcej danych na mapie Geoportal</a>`;
+        const geoLink = `<a href="https://mapy.geoportal.gov.pl/mobile/#zoom&x=${x1992}&y=${y1992}&minLevel=18&gpsPosition=true" target="blank">dodatkowe dane na mapie Geoportal</a>`;
         document.getElementById('link').innerHTML = geoLink;
     }
 };
